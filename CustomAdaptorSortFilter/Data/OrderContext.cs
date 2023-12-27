ad54a9a14ae03164fb4e46f653cb544011f4a649
BackiaRaj <backiaraj.kanagaraj@syncfusion.com>
2023-12-27 12:20:20 +0530

    Update the project

---
+++ CustomAdaptorSortFilter/Data/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomAdaptorSortFilter.Data;
+
+namespace CustomAdaptorSortFilter.Data
+{
+    public class OrderContext : DbContext
+    {
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\GitHub\SyncfusionExamples\Grid\CustomAdaptor-CRUD-Sortfilter\grid\EJ2-DataGrid-Blazor-CustomAdaptor-As-Component-CRUD-SortFilter\CustomADP\CustomADP\App_Data\NORTHWND.MDF;Integrated Security=True");
+            }
+        }
+    }
+ 
+}
